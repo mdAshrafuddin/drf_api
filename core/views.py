a3b95dd014a36c61f87b7caf304a31e2ad646f32
mdAshrafuddin <mdtanjil01753511918@gmail.com>
2021-01-21 19:43:23 +0600

    drf rest api

---
+++ core/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# third party import
+from rest_framework import mixins
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import PostSerializer
+from .models import Post
+from rest_framework import generics
+
+# Create your views here.
+
+class PostView(
+    mixins.ListModelMixin, 
+    mixins.CreateModelMixin,
+    generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class PostCreateView(mixins.ListModelMixin, generics.CreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+class PostListCreateView(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+# class TestView(APIView):
+
+# 	permission_classes = (IsAuthenticated, )
+
+
+# 	def get(self, request, *args, **kwargs):
+# 		qs = Post.objects.all()
+# 		serializer = PostSerializer(qs, many=True)
+# 		return Response(serializer.data)
+
+# 	def post(self, request, *args, **kwargs):
+# 		serializer = PostSerializer(data=request.data)
+# 		if serializer.is_valid():
+# 			serializer.save()
+# 			return Response(serializer.data)
+# 		return Response(serializer.errors)
+
+# def get(self, request, *args, **kwargs):
+	# 	data = {
+	# 		'name' : 'Ashraf',
+	# 		'age'  : 23
+	# 	}
+	# 	return Response(data)
+
+# def test_views(request):
+# 	data = {
+# 		'name' : 'Ashraf',
+# 		'age'  : 23
+# 	}
+
+# 	return JsonResponse(data)
